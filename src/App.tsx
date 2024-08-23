1c61c8a212edefe5c5da12af91ec721d2f61b06a
Sebastian Martinez <js.martinez.sandino@gmail.com>
2024-08-23 11:08:38 -0500

    fix: add routes and use pages folder instead routes folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,45 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import Router from './routes';
-import './styles/vendors.scss';
+import { Routes, Route } from 'react-router-dom';
 
-const App = () => (
-  <main>
-    <p>App Works!</p>
-    <ul>
-      <li>
-        <Link to="/">Home</Link>
-      </li>
-      <li>
-        <Link to="/about">About</Link>
-      </li>
-    </ul>
-    <Router />
-  </main>
+import './styles/globals.scss';
+
+import { UnauthenticatedRoute } from '@/components/UnauthenticatedRoute';
+import { AuthenticatedRoute } from '@/components/AuthenticatedRoute';
+import {
+  HomePage,
+  SearchPage,
+  ProfilePage,
+  LoginPage,
+  RegisterPage,
+  ForgotPasswordPage,
+  VerifyEmailPage,
+  BusinessDetailPage,
+  UnauthorizedPage,
+  NotFoundPage,
+} from '@/pages';
+
+const Router: React.FC = () => (
+  <Routes>
+    <Route path="/" element={<HomePage />} />
+    <Route path="/search" element={<SearchPage />} />
+    <Route path="/business/:id" element={<BusinessDetailPage />} />
+    <Route path="/unauthorized" element={<UnauthorizedPage />} />
+
+    <Route element={<UnauthenticatedRoute />}>
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+      <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+    </Route>
+
+    <Route element={<AuthenticatedRoute />}>
+      <Route path="/profile" element={<ProfilePage />} />
+      <Route path="/verify-email" element={<VerifyEmailPage />} />
+    </Route>
+
+    <Route path="/*" element={<NotFoundPage />} />
+  </Routes>
 );
+
+const App = () => <Router />;
 
 export default App;
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export interface HomePageProps {}
+
+export const HomePage: React.FC<HomePageProps> = () => <span>Hola</span>;
